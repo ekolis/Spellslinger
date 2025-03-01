9aabaf05a38273bba566cd18e665e1b7883a9132
Ed Kolis <edkolis@gmail.com>
2025-03-01 15:17:54 -0500

    Display colors for terrain

---
+++ Spellslinger/ViewExtensions.cs
@@ -0,0 +1,11 @@
+﻿using System.Drawing;
+
+namespace Spellslinger;
+
+public static class ViewExtensions
+{
+	public static string ToRgbaString(this Color color)
+	{
+		return $"rgba({color.R}, {color.G}, {color.B}, {color.A / 255f})";
+	}
+}
