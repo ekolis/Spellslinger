f48b2445383b524f2994c19499382552b5c896e1
Ed Kolis <edkolis@gmail.com>
2025-03-05 17:30:01 -0500

    Add HP/MP bars to actors, but they don't refresh until a turn has passed

---
+++ Spellslinger/Components/DualMeterBar.razor.cs
@@ -0,0 +1,37 @@
+﻿using System.Drawing;
+using Microsoft.AspNetCore.Components;
+using Spellslinger.Models;
+
+namespace Spellslinger.Components;
+
+public partial class DualMeterBar
+{
+	[Parameter]
+	public int InnerValue { get; set; }
+
+	[Parameter]
+	public int InnerMax { get; set; }
+
+	[Parameter]
+	public int OuterValue { get; set; }
+
+	[Parameter]
+	public int OuterMax { get; set; }
+
+	[Parameter]
+	public Color Color { get; set; }
+
+	[Parameter]
+	public int InnerWidth { get; set; }
+
+	[Parameter]
+	public int OuterWidth { get; set; }
+
+	public int Max => InnerMax + OuterMax;
+
+	protected override void OnInitialized()
+	{
+		InnerWidth = 100 * InnerValue / Max;
+		OuterWidth = 100 * OuterValue / Max;
+	}
+}
