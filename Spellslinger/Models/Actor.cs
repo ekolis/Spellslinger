3296036d1eaecfd408796c0440c46451621444f6
Ed Kolis <edkolis@gmail.com>
2025-03-07 16:01:33 -0500

    Empty spell slots are now shown on the stats view so you know you can prepare more spells

--- Spellslinger/Models/Actor.cs
+++ Spellslinger/Models/Actor.cs
@@ -156,7 +156,7 @@ public class Actor
 	/// <summary>
 	/// The maximum number of general spells that this actor can have prepared at a time.
 	/// </summary>
-	public int MaxGeneralSpells => 1 + Stats.Memory / 3;
+	public int MaxGeneralSpells => Math.Min(7, 1 + Stats.Memory / 3);
 
 	/// <summary>
 	/// The maximum number of runes that this actor can use in a single spell.
